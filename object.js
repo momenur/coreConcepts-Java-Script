c5e8e84fc86e711c4427c6005a3a26085709170c
momenur <momenurislam6@gmail.com>
2023-01-31 00:34:56 +0600

    added object

---
+++ object.js
@@ -0,0 +1,10 @@
+var mobile = {
+    brand: 'Redmi',
+    price: 14500,
+    storage: '64 GB',
+    camara: '50 MP'
+}
+console.log(mobile);
+console.log(mobile.brand);
+mobile.brand = 'Iphone';
+console.log(mobile);
--- return.js
+++ return.js
@@ -1,9 +1,17 @@
-function add(num1, num2){
-    console.log(num1, num2);
-    var sum = num1 + num2;
-    console.log(sum);
-    return sum;
-}
+// function add(num1, num2){
+//     console.log(num1, num2);
+//     var sum = num1 + num2;
+//     console.log(sum);
+//     return sum;
+// }
 
-var total = add(20, 30);
-console.log('Total', total);
+// var total = add(20, 30);
+// console.log('Total', total);
+function bringShingara(money){
+    var price = 10;
+    var quantity = money / price;
+    return quantity;
+}
+var myTaka = 150;
+var sinGaras = bringShingara(myTaka);
+console.log('Singras khabo:', sinGaras);
