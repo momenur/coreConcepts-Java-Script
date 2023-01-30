18fe7a0f430203e4ca38665512694b3ca4f6e736
momenur <momenurislam6@gmail.com>
2023-01-30 23:40:16 +0600

    added trturn

---
+++ return.js
@@ -0,0 +1,9 @@
+function add(num1, num2){
+    console.log(num1, num2);
+    var sum = num1 + num2;
+    console.log(sum);
+    return sum;
+}
+
+var total = add(20, 30);
+console.log('Total', total);
