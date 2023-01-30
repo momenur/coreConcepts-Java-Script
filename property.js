7ce5f2b20c567dba80ad47e494fbbf85085aa0ec
momenur <momenurislam6@gmail.com>
2023-01-31 01:21:42 +0600

    added object porperty and valus

---
+++ property.js
@@ -0,0 +1,11 @@
+var myComputer = {
+    brand: 'Asus',
+    RAM: '4GB',
+    HDD: '1TB',
+    price: '62000'
+}
+
+var propertys = Object.keys(myComputer);
+console.log(propertys);
+var propertyValus = Object.values(myComputer);
+console.log(propertyValus);
