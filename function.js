2886058c752d4a586c366e48c40f19e96768e285
momenur <momenurislam6@gmail.com>
2023-01-30 15:51:32 +0600

    Added Function

---
+++ function.js
@@ -0,0 +1,6 @@
+function startFan(){
+    console.log('Stand Up');
+    console.log('walk towards the Switch');
+    console.log('Press The Switch');
+}
+startFan();
---
+++ practice.js
@@ -0,0 +1,12 @@
+var fruits = ["apple", "Mango", "orange", "Guyaba", "Banana"];
+// Array Slice 
+// console.log(fruits.slice(1,3));
+
+
+// fruits.unshift("butter");
+// console.log(fruits.indexOf("butter"));
+for(i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+var Student = 'MomenurIslam';
+console.log(typeof(Student));
